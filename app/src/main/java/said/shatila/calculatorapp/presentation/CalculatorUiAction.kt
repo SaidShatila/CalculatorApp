aa6cc52613d4742d8d863f3ae1ada60908ae6de1
Shatila <SaidShatila@outlook.com>
2022-11-29 16:49:07 +0200

    added new stuff

---
+++ app/src/main/java/said/shatila/calculatorapp/presentation/CalculatorUiAction.kt
@@ -0,0 +1,19 @@
+package said.shatila.calculatorapp.presentation
+
+import androidx.compose.runtime.Composable
+import said.shatila.calculatorapp.domain.CalculatorAction
+
+data class CalculatorUiAction(
+    val text: String?,
+    val highlightLevel: HighlightLevel,
+    val action: CalculatorAction,
+    val content: @Composable () -> Unit = {}
+)
+
+
+sealed interface HighlightLevel {
+    object Neutral : HighlightLevel
+    object SemiHighlighted : HighlightLevel
+    object Highlighted : HighlightLevel
+    object StronglyHighlighted : HighlightLevel
+}
