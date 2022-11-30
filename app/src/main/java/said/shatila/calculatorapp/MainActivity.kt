7ab23c762ff3e7f9f0a97c354570352cdd25ec59
Shatila <SaidShatila@outlook.com>
2022-11-30 14:27:16 +0200

    different variant build commentes

--- app/src/main/java/said/shatila/calculatorapp/MainActivity.kt
+++ app/src/main/java/said/shatila/calculatorapp/MainActivity.kt
@@ -3,14 +3,13 @@ package said.shatila.calculatorapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.saidshatila.materialcalculator.Constants
 import said.shatila.calculatorapp.presentation.CalculatorScreen
 import said.shatila.calculatorapp.ui.theme.MaterialCalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Constants.BASE_URL
+//        Constants.BASE_URL
         setContent {
             MaterialCalculatorTheme {
                 // A surface container using the 'background' color from the theme
